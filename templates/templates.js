43658b00d9d45b47798cd9105d55ea711ab6b0c0
garcierl <garcierl@iu.edu>
2020-09-22 19:51:48 -0400

    Update templates.js

--- templates/templates.js
+++ templates/templates.js
@@ -16,13 +16,16 @@ class vendingmachine {
     buy() {
         this.item --;
     }
+    
+        
+    
 }
 
 let firstCandy = new vendingmachine("Hershey");
 let itemDiv =document.getElementById("itemDiv");
 itemDiv.innerHTML = firstCandy.render();
 
-function BuyItem() {
+function BuyItem1() {
     firstCandy.buy();
     itemDiv.innerHTML = firstCandy.render()
 }
@@ -31,7 +34,7 @@ let SecondCandy = new vendingmachine("M&M");
 let item3Div =document.getElementById("item3Div");
 item3Div.innerHTML = SecondCandy.render();
 
-function BuyItem() {
+function BuyItem2() {
     SecondCandy.buy();
     item3Div.innerHTML = SecondCandy.render()
 } 
@@ -40,7 +43,7 @@ let firstChips = new vendingmachine("Lays");
 let item2Div =document.getElementById("item2Div");
 item2Div.innerHTML = firstChips.render();
 
-function BuyItem() {
+function BuyItem3() {
     firstChips.buy();
     item2Div.innerHTML = firstChips.render()
 }
