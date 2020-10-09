4d224f3288d8bad14b7efd290caacc48e872ec7f
garcierl <garcierl@iu.edu>
2020-10-09 14:30:53 -0400

    Create app.js

---
+++ animation/app.js
@@ -0,0 +1,42 @@
+gsap.fromTo("#blueRect",{scale:0},{scale: 1, duration: 1.5})
+
+gsap.fromTo("#purple_rect1",{scale:0},{scale: 1, duration: 1.5})
+gsap.fromTo("#purple_rect2",{scale:0},{scale: 1, duration: 1.5})
+gsap.fromTo("#purple_rect3",{scale:0},{scale: 1, duration: 1.5})
+gsap.fromTo("#purple_rect4",{scale:0},{scale: 1, duration: 1.5})
+gsap.fromTo("#purple_rect5",{scale:0},{scale: 1, duration: 1.5})
+gsap.fromTo("#purple_rect6",{scale:0},{scale: 1, duration: 1.5})
+
+function mouseover1(){
+gsap.fromTo("#blueRect",{opacity:1},{opacity: 0.5, duration: 1.5})
+}
+function mouseover2(){
+gsap.fromTo("#purple_rect1",{opacity:1},{opacity: 0.5, duration: 1.5})
+}
+function mouseover3(){
+    gsap.fromTo("#purple_rect2",{opacity:1},{opacity: 0.5, duration: 1.5})
+}
+function mouseover4(){
+gsap.fromTo("#purple_rect3",{opacity:1},{opacity: 0.5, duration: 1.5})
+}
+function mouseover5(){
+    gsap.fromTo("#purple_rect4",{opacity:1},{opacity: 0.5, duration: 1.5})
+}
+function mouseover6(){
+    gsap.fromTo("#purple_rect5",{opacity:1},{opacity: 0.5, duration: 1.5})
+}
+function mouseover7(){    
+gsap.fromTo("#purple_rect6",{opacity:1},{opacity: 0.5, duration: 1.5})
+}
+
+
+function mouseclick(){
+    gsap.fromTo("#purple_rect1",{scale:1},{scale: 0, duration: 1.5})
+    gsap.fromTo("#purple_rect2",{scale:1},{scale: 0, duration: 1.5})
+    gsap.fromTo("#purple_rect3",{scale:1},{scale: 0, duration: 1.5})
+    gsap.fromTo("#purple_rect4",{scale:1},{scale: 0, duration: 1.5})
+    gsap.fromTo("#purple_rect5",{scale:1},{scale: 0, duration: 1.5})
+    gsap.fromTo("#purple_rect6",{scale:1},{scale: 0, duration: 1.5})
+    gsap.fromTo("#blueRect",{scale:1},{scale: 0, duration: 1.5})
+}
+
